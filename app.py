7dd0d687465e1aee2d0868aa17ba57169ee25f91
Sameena <sameenafathima99999@gmail.com>
2020-09-11 13:18:04 +0530

    UI and generating link added

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from fpdf import FPDF, HTMLMixin
-from flask import Flask, jsonify, request, redirect, render_template,session,url_for
+from flask import Flask, jsonify, request, redirect, render_template,session,url_for,send_file
 from flask_pymongo import PyMongo
 import string
 from bson.objectid import ObjectId
+import secrets
 
-# class HTML2PDF(FPDF, HTMLMixin):
-#     pass
+
+class HTML2PDF(FPDF, HTMLMixin):
+    pass
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://sameena:testpass@cluster0.pdqrp.mongodb.net/form_automation?retryWrites=true&w=majority"
-#app.config["MONGO_URI"] = "mongodb+srv://teat_user:test123@cluster0.pdqrp.mongodb.net/hospital_pricing?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 secret_admirer = mongo.db.secret_admirer
 
-@app.route("/secret-admirer")
-def secret_admirer():
+@app.route("/secret-admirer/<form_name>/<referral_number>",methods=['POST','GET'])
+def secret_admirer_form(form_name,referral_number):
     return render_template("form.html")
 
 @app.route("/secret-admirer/submit",methods = ['POST'])
@@ -26,23 +27,69 @@ def insert_data():
     'reason': request.form['reason'],
     'hint' : request.form['hint']}
     secret_admirer.insert_one(form_data)
+    return "submitted successfully"
 
+@app.route("/download-pdf",methods = ['GET'])
+def download_pdf():
+    return render_template('download.html',value = "secret_admirer.pdf")
+
+pdf = HTML2PDF()
+@app.route('/return-file/<filename>')
+def return_files_tut(filename):
+    rows = secret_admirer.find()
+    results = []
+    for row in rows:
+        Secret_Admirer = row['secret_admirer']
+        Reason = row['reason']
+        Hint = row['hint']
+        print("printing",Secret_Admirer,Reason,Hint)
+        html = '''<h1>WHO is your secret admirer?</h1>
+            <p>'''+Secret_Admirer+'''</p>
+            <h1>Why?</h1>
+            <p>'''+Reason+'''</p>
+            <h1>Hint</h1>
+            <p>'''+Hint+'''</p>'''
+        print(html)
+        pdf.add_page()
+        pdf.write_html(html)
+    pdf.output('img/secret_admirer.pdf')
+    file_path = "img/secret_admirer.pdf"
+    return send_file(file_path, as_attachment=True, attachment_filename='')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/new-form')
+def new_form():
+    secretsGenerator = secrets.SystemRandom()
+    randomNumber = secretsGenerator.randint(9999,1000000)
+    print(randomNumber)
+    return render_template("new_form.html",rand_num = randomNumber)
+# @app.route("/htmltopdf",methods = ['GET','POST'])
 # def html2pdf():
 #     # with open('index.html', 'r') as f:
 #     #     html_string = f.read() 
-#     html = ''' <h1>WHO is your secret admirer?</h1>
-#         <p>Gokul</p>
-#         <h1>Why?</h1>
-#         <p>Hardworking and dedicated</p>
-#         <h1>Hint</h1>
-#         <p>Deja vu</p>'''
-    
-#     pdf = HTML2PDF()
-#     pdf.add_page()
-#     pdf.write_html(html)
-#     pdf.add_page()
-#     pdf.write_html(html)
-#     pdf.output('html2pdf.pdf')
+#     rows = secret_admirer.find()
+#     results = []
+#     for row in rows:
+#         Secret_Admirer = row['secret_admirer']
+#         Reason = row['reason']
+#         Hint = row['hint']
+#         print("printing",Secret_Admirer,Reason,Hint)
+#         html = '''<h1>WHO is your secret admirer?</h1>
+#             <p>'''+Secret_Admirer+'''</p>
+#             <h1>Why?</h1>
+#             <p>'''+Reason+'''</p>
+#             <h1>Hint</h1>
+#             <p>'''+Hint+'''</p>'''
+#         print(html)
+#         pdf.add_page()
+#         pdf.write_html(html)
+#     pdf.output('img/secret_admirer.pdf')
+#     file_path = "img/secret_admirer.pdf"
+#     return send_file(file_path, as_attachment=True, attachment_filename='')
     
 if __name__ == '__main__':
     app.run(debug=True)
